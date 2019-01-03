596682412cb5f13050e5cd925993154f35d5da5f
Prabhakar Mudliyar <mudliyarp@HCL.COM>
2019-01-03 16:32:44 +0530

    Navigation, Grid Using Flex and Card design done

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open-sans:400,500" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+        <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body>
+        <header class="section">
+            <div class="container">
+                <div class="header-top">
+                    <h1>Timesheet</h1>
+                    <a href="">Login</a>
+                </div>
+                <nav>
+                    <a hreg="#">Program</a>
+                    <a hreg="#">Project</a>
+                    <a hreg="#">Tasks</a>
+                    <a hreg="#">Settings</a>
+                </nav>
+            </div>
+        </header>
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="box">
+                            <div class="card">
+                                <div class="card-left">
+                                    <a href="#" class="card-skill-button">PHP</a>
+                                    <img src="https://laracasts.com/images/series/2018/the-php-practitioner.svg">
+                                    <span class="card-difficulty">Intermediate Difficulty</span>
+                                </div>
+                                <div class="card-right">
+                                    <h1 class="card-title">Testing Jargon</h1>
+                                    <p class="card-excerpt">Lorem Ipsum aslut itns Lorem Ipsum aslut itns Lorem Ipsum aslut itns Lorem Ipsum aslut itns</p>
+                                    <div class="card-meta">
+                                        <div>
+                                            <img src="https://laracasts.com/images/icons/books.svg">
+                                            5 lessons
+                                        </div>
+                                        <div>
+                                            <img src="https://laracasts.com/images/icons/clock.svg">
+                                            53:10 min
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="box">
+                            <div class="card">
+                                <div class="card-left">
+                                    <a href="#" class="card-skill-button">PHP</a>
+                                    <img src="https://laracasts.com/images/series/2018/the-php-practitioner.svg">
+                                    <span class="card-difficulty">Intermediate Difficulty</span>
+                                </div>
+                                <div class="card-right">
+                                    <h1 class="card-title">Testing Jargon</h1>
+                                    <p class="card-excerpt">Lorem Ipsum aslut itns Lorem Ipsum aslut itns Lorem Ipsum aslut itns Lorem Ipsum aslut itns</p>
+                                    <div class="card-meta">
+                                        <div>
+                                            <img src="https://laracasts.com/images/icons/books.svg">
+                                            5 lessons
+                                        </div>
+                                        <div>
+                                            <img src="https://laracasts.com/images/icons/clock.svg">
+                                            53:10 min
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                    <div class="col">
+                        <div class="box">Some Content Here</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
